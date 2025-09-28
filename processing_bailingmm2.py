017f111728c5ce4ed938b4ec6a52a72610b0453b
weilong.cwl <weilong.cwl@antgroup.com>
2025-09-28 20:44:13 +0800

    add image gen

--- processing_bailingmm2.py
+++ processing_bailingmm2.py
@@ -28,14 +28,14 @@ else:
 
 from transformers.feature_extraction_utils import BatchFeature
 from transformers.image_utils import ImageInput
-from transformers.video_utils import VideoInput
 from transformers.processing_utils import (
     ProcessingKwargs,
     ProcessorMixin,
 )
 from transformers.tokenization_utils_base import PreTokenizedInput, TextInput
 
-from bailingmm_utils import process_vision_info
+from bailingmm_utils import process_vision_info, VideoInput, process_ratio
+import torchvision
 
 DEFAULT_IMAGE_PATCH_TOKEN = "<imagePatch>"
 DEFAULT_IM_START_TOKEN = "<image>"
@@ -194,11 +194,23 @@ class BailingMM2Processor(ProcessorMixin):
         image_inputs = {}
         video_inputs = {}
         audio_inputs = {}
+        image_gen_inputs = {}
 
         if images is not None:
             image_inputs = self.image_processor(images=images, videos=None, **output_kwargs["images_kwargs"])
             image_grid_thw = image_inputs["image_grid_thw"]
             text = self._expand_image_tokens(text, image_grid_thw)
+            ref_pil = images[0] if isinstance(images, list) else images
+            ref_pil = ref_pil.convert("RGB")
+            closest_size, resize_size = process_ratio(ori_h=ref_pil.size[1], ori_w=ref_pil.size[0])
+            ref_pil = torchvision.transforms.functional.resize(ref_pil, resize_size, interpolation=torchvision.transforms.InterpolationMode.BILINEAR)
+            ref_pil = torchvision.transforms.functional.center_crop(ref_pil, closest_size)
+            ref_tensor = ((torchvision.transforms.functional.to_tensor(ref_pil) - 0.5) * 2.0).unsqueeze(0)
+            image_gen_inputs = {
+                "image_gen_pixel_values_reference": ref_tensor,
+                "image_gen_height": torch.LongTensor([ref_pil.size[1]]),
+                "image_gen_width": torch.LongTensor([ref_pil.size[0]]),
+            }
 
         if videos is not None:
             video_inputs = self.image_processor(images=None, videos=videos, do_resize=False, **output_kwargs["videos_kwargs"])
@@ -224,7 +236,7 @@ class BailingMM2Processor(ProcessorMixin):
             audio_inputs["audio_placeholder_loc_lens"] = torch.tensor(loc_lens, dtype=torch.long)
             audio_inputs.pop('encoder_feats_lengths')
 
-        return BatchFeature(data={**text_inputs, **image_inputs, **video_inputs, **audio_inputs})
+        return BatchFeature(data={**text_inputs, **image_inputs, **video_inputs, **audio_inputs, **image_gen_inputs})
 
     def apply_system_template(self, sys_prompt_exp=None, use_cot_system_prompt=False):
         if use_cot_system_prompt:
--- test_infer_gen_image.py
+++ test_infer_gen_image.py
@@ -1,110 +1,113 @@
 import os
-import time
 import torch
-from transformers import AutoProcessor
+import time
+import numpy as np
+from bisect import bisect_left
 
-from modeling_bailingmm import BailingMMNativeForConditionalGeneration
 from IPython import embed
-import torchvision
-from PIL import Image
-import re
 
-import torch.nn as nn
-from collections import defaultdict
-from bailingmm_utils import process_ratio
+from transformers import (
+    AutoProcessor,
+)
 
-def auto_translate(model, processor, text):
-    if re.search(r'[\u4e00-\u9fff]', text):
-        prefix = "Translate the Chinese phrase below into natural English. Return only the translation result without any explanations, prefixes, or formatting. Phrase to translate:"
-        text = f"{prefix}{text}"
-        messages = [
-            {
-                "role": "HUMAN",
-                "content": [
-                    {"type": "text", "text": text},
-                ],
-            }
-        ]
-        text = processor.apply_chat_template(messages, add_generation_prompt=True)
-        image_inputs, video_inputs, audio_inputs = processor.process_vision_info(messages)
-        inputs = processor(
-            text=[text],
-            images=image_inputs,
-            videos=video_inputs,
-            audios=audio_inputs,
-            return_tensors="pt",
-        ).to(model.device)
+from modeling_bailingmm2 import BailingMM2NativeForConditionalGeneration
 
-        for k in inputs.keys():
-            if k == "pixel_values" or k == "pixel_values_videos" or k == "audio_feats":
-                inputs[k] = inputs[k].to(dtype=torch.bfloat16)
-        
-        #srt_time = time.time()
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+def split_model():
+    device_map = {}
+    world_size = torch.cuda.device_count()
+    num_layers = 32
+    layer_per_gpu = num_layers // world_size
+    layer_per_gpu = [i * layer_per_gpu for i in range(1, world_size + 1)]
+    for i in range(num_layers):
+        device_map[f'model.model.layers.{i}'] = bisect_left(layer_per_gpu, i)
+
+    device_map['vision'] = 0
+    device_map['audio'] = 0
+    device_map['linear_proj'] = 0
+    device_map['linear_proj_audio'] = 0
+    device_map['model.model.word_embeddings.weight'] = 0
+    device_map['model.model.norm.weight'] = 0
+    device_map['model.lm_head.weight'] = 0
+    device_map['model.model.norm'] = 0
+    device_map[f'model.model.layers.{num_layers - 1}'] = 0
+    return device_map
+
+def generate(messages, processor, model, sys_prompt_exp=None, use_cot_system_prompt=False, max_new_tokens=512):
+    text = processor.apply_chat_template(
+        messages, 
+        sys_prompt_exp=sys_prompt_exp,
+        use_cot_system_prompt=use_cot_system_prompt
+    )
+    image_inputs, video_inputs, audio_inputs = processor.process_vision_info(messages)
+
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        audios=audio_inputs,
+        return_tensors="pt",
+    ).to(model.device)
+
+    for k in inputs.keys():
+        if k == "pixel_values" or k == "pixel_values_videos" or k == "audio_feats":
+            inputs[k] = inputs[k].to(dtype=torch.bfloat16)
+
+    srt_time = time.time()
+
+    with torch.no_grad():
         generated_ids = model.generate(
             **inputs,
-            max_new_tokens=128,
-            use_cache=False,
+            max_new_tokens=max_new_tokens,
+            use_cache=True,
             eos_token_id=processor.gen_terminator,
+            num_logits_to_keep=1,
         )
-        generated_ids_trimmed = [
-                out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-            ]
-        text = processor.batch_decode(
-            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-        )[0]
 
-    return text
+    end_time = time.time()
 
-def generate_gen_image(
-    model,
-    processor,
-    prompt,
-    height=None,
-    width=None,
-    input_image_path=None,
-    use_auto_translate=True,
-    debug=False,
-    POSITIVE_PREFIX_T2I=None,
-    POSITIVE_PREFIX_I2I="high quality",
-    NEGATIVE_PREFIX="worst quality, low quality, bad eyes, bad iris, twisted face, blurry, bad hand, watermark, multiple limbs, deformed fingers, bad fingers, ugly, monochrome, horror, geometry, bad anatomy, bad limbs, Blurry pupil, bad shading, error, bad composition, Extra fingers, strange fingers, Extra ears, extra leg, bad leg, disability, Blurry eyes, bad eyes, Twisted body, confusion, bad legs",
-    image_gen_steps=30,
-):
-    # input_image_path 设置为 None, 运行 文生图，否则 图生图
-    if height is None or width is None:
-        image_gen_width, image_gen_height = 512 * 1, 512 * 1
-    else:
-        image_gen_width, image_gen_height = width, height
+    generated_ids_trimmed = [
+        out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
 
-    closest_size, _ = process_ratio(ori_h=image_gen_height, ori_w=image_gen_width)
-    image_gen_height, image_gen_width = closest_size[0] * 1, closest_size[1] * 1
+    # tps = generated_ids.shape[1] / (end_time - srt_time)
+    # print(f"generated {generated_ids.shape[1]} tokens in {end_time - srt_time:.2f} seconds, tokens per second: {tps:.2f} tokens/s")
 
-    if use_auto_translate:
-        prompt_ori = prompt
-        prompt = auto_translate(model, processor, prompt)
-        if debug:
-            print("prompt: {} -> translated: {}".format(prompt_ori, prompt))
-    
-    if input_image_path is not None:
-        if POSITIVE_PREFIX_I2I:
-            prompt = "{}; Requirement: {}".format(prompt, POSITIVE_PREFIX_I2I)
-    else:
-        if POSITIVE_PREFIX_T2I:
-            prompt = "{}; Requirement: {}".format(prompt, POSITIVE_PREFIX_T2I)
-    
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )[0]
+
+    return output_text
+
+if __name__ == '__main__':
+    model_name_or_path = "/nativemm/share/cpfs/weilong.cwl/checkpoints/Ming_Flash_2.0_sft1_merged"
+    #"/input/sunyunxiao.syx/checkpoints/Ming_Flash_2.0_sft1/"
+    model = BailingMM2NativeForConditionalGeneration.from_pretrained(
+        model_name_or_path,
+        torch_dtype=torch.bfloat16,
+        attn_implementation="flash_attention_2",
+        device_map=split_model(),
+        load_image_gen=True,
+    ).to(dtype=torch.bfloat16)
+
+    processor = AutoProcessor.from_pretrained("./", trust_remote_code=True)
+
+    # gen_input_pixels = 451584
+    # processor.image_processor.max_pixels = gen_input_pixels
+    # processor.image_processor.min_pixels = gen_input_pixels
+
     messages = [
         {
             "role": "HUMAN",
             "content": [
-                {"type": "text", "text": prompt},
-            ] if input_image_path is None else [
-                {"type": "image", "image": input_image_path},
-                {"type": "text", "text": prompt},
+                {"type": "text", "text": "Draw a beautiful girl with short black hair and red dress."},
             ],
         }
     ]
-    if debug:
-        print("messages:", messages)
-    
 
     text = processor.apply_chat_template(messages, add_generation_prompt=True)
     image_inputs, video_inputs, audio_inputs = processor.process_vision_info(messages)
@@ -117,97 +120,121 @@ def generate_gen_image(
         return_tensors="pt",
     ).to(model.device)
 
-    if "image_gen_height" in inputs:
-        image_gen_height = inputs["image_gen_height"]
-        del inputs["image_gen_height"]
-    
-    if "image_gen_width" in inputs:
-        image_gen_width = inputs["image_gen_width"]
-        del inputs["image_gen_width"]
-
     for k in inputs.keys():
-        if k == "pixel_values" or k == "pixel_values_videos" or k == "audio_feats":
+        if k in ["pixel_values", "pixel_values_videos", "audio_feats", "pixel_values_reference"]:
             inputs[k] = inputs[k].to(dtype=torch.bfloat16)
 
-    if NEGATIVE_PREFIX:
-        negative_messages = [
-            {
-                "role": "HUMAN",
-                "content": [
-                    {"type": "text", "text": NEGATIVE_PREFIX},
-                ] if input_image_path is None else [
-                    {"type": "image", "image": input_image_path},
-                    {"type": "text", "text": NEGATIVE_PREFIX},
-                ],
-            }
-        ]
-        if debug:
-            print("negative_messages:", negative_messages)
-
-        negative_text = processor.apply_chat_template(negative_messages, add_generation_prompt=True)
-        negative_inputs = processor(
-            text=[negative_text],
-            images=image_inputs,
-            videos=None,
-            audios=None,
-            return_tensors="pt",
-        ).to(model.device)
-        inputs["image_gen_negative_input_ids"] = negative_inputs["input_ids"]
-        inputs["image_gen_negative_attention_mask"] = negative_inputs["attention_mask"]
-
+    # set `image_gen=True` to enable image generation
     image = model.generate(
         **inputs,
-        max_new_tokens=128,
-        use_cache=False,
-        eos_token_id=processor.gen_terminator,
         image_gen=True,
-        image_gen_height=image_gen_height,
-        image_gen_width=image_gen_width,
-        image_gen_steps=image_gen_steps,
     )
+
+    image.save("./t2i_girl.jpg")
     
-    return image
 
-
-if __name__ == '__main__':
-    
-    model_path = "inclusionAI/Ming-Lite-Omni"
-    processor = AutoProcessor.from_pretrained('.', trust_remote_code=True)
-    model = BailingMMNativeForConditionalGeneration.from_pretrained(
-        model_path,
-        torch_dtype=torch.bfloat16,
-        attn_implementation="flash_attention_2",
-        load_image_gen=True,
-    ).to("cuda")
-
-    gen_input_pixels = 451584
-    processor.image_processor.max_pixels = gen_input_pixels
-    processor.image_processor.min_pixels = gen_input_pixels
-    
-    image = generate_gen_image(
-        model=model, 
-        processor=processor,
-        prompt="a beautiful girl wearing a red dress.",
-        POSITIVE_PREFIX_T2I="",
-        POSITIVE_PREFIX_I2I="",
-        image_gen_steps=30,
-        NEGATIVE_PREFIX="",
-    )
-    image.save("./woman_red.jpg")
-
-    image = generate_gen_image(
-        model=model, 
-        processor=processor,
-        prompt="给人物戴上墨镜",
-        input_image_path="./woman_red.jpg",
-        POSITIVE_PREFIX_T2I="",
-        POSITIVE_PREFIX_I2I="",
-        image_gen_steps=30,
-        NEGATIVE_PREFIX="",
-        use_auto_translate=False,
-    )  
-    image.save("./woman_red_sunglasses.jpg")
+    print("Instruction: Draw a beautiful girl with short black hair and red dress.")
 
     
+    vision_path = "/input/sunyunxiao.syx/assets/"
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "image", "image": os.path.join(vision_path, "flowers.jpg")},
+                {"type": "text", "text": "What kind of flower is this?"},
+            ],
+        }
+    ]
+
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=512)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "text", "text": "请介绍下你自己"}
+            ],
+        }
+    ]
+
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=512)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "video", "video": os.path.join(vision_path, "yoga.mp4")},
+                {"type": "text", "text": "What is the woman doing?"},
+            ],
+        }
+    ]
     
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=512)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "text", "text": "中国的首都是哪里？"},
+            ],
+        },
+        {
+            "role": "ASSISTANT",
+            "content": [
+                {"type": "text", "text": "北京"},
+            ],
+        },
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "text", "text": "它的占地面积是多少？有多少常住人口？"},
+            ],
+        },
+    ]
+
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=512)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "text", "text": "请详细介绍鹦鹉的生活习性。"}
+            ],
+        }
+    ]
+
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=8192, use_cot_system_prompt=True)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
+
+
+    messages = [
+        {
+            "role": "HUMAN",
+            "content": [
+                {"type": "video", "video": os.path.join(vision_path, "yoga.mp4"), "max_frames": 40, "sample": "uniform"},
+                {"type": "image", "image": os.path.join(vision_path, "flowers.jpg")},
+                {"type": "text", "text": "What is the woman doing in the video and what kind of flower is in the image?"},
+            ],
+        }
+    ]
     
+    srt_time = time.time()
+    output_text = generate(messages, processor=processor, model=model, max_new_tokens=512)
+    print(output_text)
+    print(f"Generate time: {(time.time() - srt_time):.2f}s")
